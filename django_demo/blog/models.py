911d0e82e9fac71225b2385b8b80a4c9652935ca
Василий <vasyachemax@yandex.ru>
2017-10-26 13:33:19 +0500

    Добавлены две модели, Questions и Choices, добавлены миграции для них

--- django_demo/blog/models.py
+++ django_demo/blog/models.py
@@ -15,3 +15,14 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Questions(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Questions, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
