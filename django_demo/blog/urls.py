5d16cecb89d60e0d40c752440a2f3f4159adf249
Василий <vasyachemax@yandex.ru>
2017-10-30 04:43:50 +0500

    Именованные урлы

--- django_demo/blog/urls.py
+++ django_demo/blog/urls.py
@@ -4,8 +4,8 @@ from .views.questions import detail, result, vote, all_question
 
 urlpatterns = [
     url(r'^$', post_list, name='post_list'),
-    url(r'^question/$', all_question),
-    url(r'^question/(?P<question_id>[0-9]+)/$', detail),
-    url(r'^question/(?P<question_id>[0-9]+)/results/$', result),
-    url(r'^question/(?P<question_id>[0-9]+)/vote/(?P<choice_id>[0-9])+/$', vote),
+    url(r'^question/$', all_question, name='question'),
+    url(r'^question/(?P<question_id>[0-9]+)/$', detail, name='question_detail'),
+    url(r'^question/(?P<question_id>[0-9]+)/results/$', result, name='vote_result'),
+    url(r'^question/(?P<question_id>[0-9]+)/vote/(?P<choice_id>[0-9])+/$', vote, name='choice_vote'),
 ]
