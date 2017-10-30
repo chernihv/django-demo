e6b66ed7fc87e72bb4468523b87418dfb6116f74
Василий <vasyachemax@yandex.ru>
2017-10-31 02:43:49 +0500

    Два дополнительных метода для тестирования метода модели

--- django_demo/my_site/tests.py
+++ django_demo/my_site/tests.py
@@ -13,3 +13,13 @@ class QuestionMethodTest(TestCase):
         time = timezone.now() + datetime.timedelta(days=30)
         future_question = Question(pub_date=time)
         self.assertEqual(future_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_old_question(self):
+        time = timezone.now() - datetime.timedelta(days=30)
+        old_question= Question(pub_date=time)
+        self.assertEqual(old_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_normal_question(self):
+        time = timezone.now() - datetime.timedelta(hours=1)
+        normal_question = Question(pub_date=time)
+        self.assertEqual(normal_question.was_published_recently(), True)
