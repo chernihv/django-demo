f4c411aa50031cd494d90ff195bedf912ee925e1
Василий <vasyachemax@yandex.ru>
2017-10-31 03:33:12 +0500

    Добавлены тесты для представлений

--- django_demo/my_site/tests.py
+++ django_demo/my_site/tests.py
@@ -2,6 +2,36 @@ import datetime
 from django.utils import timezone
 from django.test import TestCase
 from .models import Question
+from django.core.urlresolvers import reverse
+
+
+def create_question(question_text: str, days: int):
+    time = timezone.now() + datetime.timedelta(days=days)
+    return Question.objects.create(question_text=question_text, pub_date=time)
+
+
+class QuestionViewTests(TestCase):
+    def test_index_view_with_no_questions(self):
+        response = self.client.get(reverse('my_site:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertQuerysetEqual(response.context['latest_question_list'], [])
+
+    def test_index_view_with_a_past_question(self):
+        q = create_question(question_text='Test question', days=-30)
+        response = self.client.get(reverse('my_site:index'))
+        self.assertQuerysetEqual(
+            response.context['latest_question_list'],
+            ['<Question: ({id}) Question text: {text}>'.format(id=q.id, text=q.question_text)],
+        )
+
+    def test_index_view_with_a_future_and_old_question(self):
+        create_question('Future question', days=5)
+        q = create_question('Old question', days=-5)
+        response = self.client.get(reverse('my_site:index'))
+        self.assertQuerysetEqual(
+            response.context['latest_question_list'],
+            ['<Question: ({id}) Question text: {text}>'.format(id=q.id, text=q.question_text)],
+        )
 
 
 class QuestionMethodTest(TestCase):
@@ -16,7 +46,7 @@ class QuestionMethodTest(TestCase):
 
     def test_was_published_recently_with_old_question(self):
         time = timezone.now() - datetime.timedelta(days=30)
-        old_question= Question(pub_date=time)
+        old_question = Question(pub_date=time)
         self.assertEqual(old_question.was_published_recently(), False)
 
     def test_was_published_recently_with_normal_question(self):
