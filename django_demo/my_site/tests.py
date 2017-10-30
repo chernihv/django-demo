23df412f007c33871398fef7af3dbe51d9009eae
Василий <vasyachemax@yandex.ru>
2017-10-31 02:36:27 +0500

    Первый Unit тест

--- django_demo/my_site/tests.py
+++ django_demo/my_site/tests.py
@@ -1,3 +1,15 @@
+import datetime
+from django.utils import timezone
 from django.test import TestCase
+from .models import Question
 
-# Create your tests here.
+
+class QuestionMethodTest(TestCase):
+    def test_was_published_recently_with_future_question(self):
+        """
+        Testing method was_published_recently() in class Question
+        :return:
+        """
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(pub_date=time)
+        self.assertEqual(future_question.was_published_recently(), False)
