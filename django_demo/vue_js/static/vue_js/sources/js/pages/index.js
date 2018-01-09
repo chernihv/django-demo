4ce3ec4420720e7d3b02b3b542b4601345973d89
Василий <vasyachemax@yandex.ru>
2018-01-09 12:48:00 +0500

    Всякие изменения

--- django_demo/vue_js/static/vue_js/sources/js/pages/index.js
+++ django_demo/vue_js/static/vue_js/sources/js/pages/index.js
@@ -10,7 +10,6 @@
     });
 
     initRealTimeChart();
-    initDonutChart();
     initSparkline();
 });
 
@@ -65,33 +64,6 @@ function initSparkline() {
     $(".sparkline").each(function () {
         var $this = $(this);
         $this.sparkline('html', $this.data());
-    });
-}
-
-function initDonutChart() {
-    Morris.Donut({
-        element: 'donut_chart',
-        data: [{
-            label: 'Chrome',
-            value: 37
-        }, {
-            label: 'Firefox',
-            value: 30
-        }, {
-            label: 'Safari',
-            value: 18
-        }, {
-            label: 'Opera',
-            value: 12
-        },
-        {
-            label: 'Other',
-            value: 3
-        }],
-        colors: ['rgb(233, 30, 99)', 'rgb(0, 188, 212)', 'rgb(255, 152, 0)', 'rgb(0, 150, 136)', 'rgb(96, 125, 139)'],
-        formatter: function (y) {
-            return y + '%'
-        }
     });
 }
 
